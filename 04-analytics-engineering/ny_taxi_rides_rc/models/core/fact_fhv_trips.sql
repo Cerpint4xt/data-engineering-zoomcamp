740fbc82306859f07c79636990930bba019c399c
rcatagrundy@gmail.com <rcatagrundy@gmail.com>
2024-02-21 04:29:58 +0000

    question3 answered with all filters and joins apply

---
+++ 04-analytics-engineering/ny_taxi_rides_rc/models/core/fact_fhv_trips.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with fhv_tripdata_rc as (
+    select *
+    from {{ ref('stg_fhv_tripdata_rc') }}
+    where pickup_locationid is not null and dropoff_locationid is not null 
+),  
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
+select 
+    fhv_tripdata_rc.pickup_datetime as pickup_datetime,
+    fhv_tripdata_rc.dropOff_datetime as dropoff_datetime,
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone, 
+    dropoff_zone.borough as dropoff_borough, 
+    dropoff_zone.zone as dropoff_zone  
+from fhv_tripdata_rc
+inner join dim_zones as pickup_zone
+on fhv_tripdata_rc.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_tripdata_rc.dropoff_locationid = dropoff_zone.locationid
--- 04-analytics-engineering/ny_taxi_rides_rc/models/staging/stg_fhv_tripdata_rc.sql
+++ 04-analytics-engineering/ny_taxi_rides_rc/models/staging/stg_fhv_tripdata_rc.sql
@@ -1,27 +1,27 @@
 {{ config(materialized='view') }}
  
-with tripdata as 
+with fhvdata as 
 (
   select *,
     row_number() over(partition by pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata_rc') }}
-  where pickup_datetime between '2019-01-01' AND '2019-12-31' 
+  where pickup_datetime between '2019-01-01 00:00:00' AND '2019-12-31 23:59:59' 
 )
 select
    -- identifiers
+   --{{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as fhvid,
    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
    
    -- timestamps
-   cast("pickup_datetime" as timestamp) as pickup_datetime,
-   cast("dropOff_datetime" as timestamp) as dropoff_datetime,
+   pickup_datetime as pickup_datetime,
+   dropOff_datetime as dropoff_datetime,
 
-   dispatching_base_num,
-   "SR_Flag" as sr_flag,
-   "Affiliated_base_number" as affiliated_base_number
+   cast(dispatching_base_num as string) as dispatching_base_num,
+   cast("SR_Flag" as string) as sr_flag,
+   cast("Affiliated_base_number" as string) as affiliated_base_number
     
-from tripdata
-where rn = 1
+from fhvdata
 
 -- dbt build --select <model.sql> --vars '{'is_test_run: false}'
 {% if var('is_test_run', default=true) %}
